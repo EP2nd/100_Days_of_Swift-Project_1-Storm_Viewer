cfe742436f15cb5f57eaa52e7ae2637e6caad7cf
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-02-21 22:23:58 +0100

    Revert "Guess_the_Flag.Project-3"
    This reverts commit 84d350ec2e2d1751536f8aeff17d342eae3016c5.

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -12,8 +12,6 @@ class DetailViewController: UIViewController {
         
         title = "Picture \(selectedPictureNumber) of \(totalPictures)"
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
-        
         navigationItem.largeTitleDisplayMode = .never
 
         if let imageToLoad = selectedImage {
@@ -30,17 +28,7 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-    
-    @objc func shareTapped() {
-        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
-            print("No image found.")
-            return
-        }
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
-        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        present(vc, animated: true)
-    }
-    
+
     /*
     // MARK: - Navigation
 
