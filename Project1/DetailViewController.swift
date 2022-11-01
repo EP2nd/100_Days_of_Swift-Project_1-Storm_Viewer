bd8ce3558ca222f0e92bb31e3e905b6d17d5e784
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-11-01 01:13:18 +0100

    Revert "Solution to the third challenge assigned Project 27"
    This reverts commit 3df453465572a421949391e964efd69e7b3d3c1b.

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -40,41 +40,13 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-    // Project 27, challenge 3:
-    func watermarked(image: UIImage) -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: image.size)
-        
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .left
-        
-        let stringAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 16),
-            .paragraphStyle: paragraphStyle,
-            .strokeColor: UIColor.black,
-            .backgroundColor: UIColor.white
-        ]
-        
-        let renderedImage = renderer.image { ctx in
-            let watermark = "From Storm Viewer"
-            let attributedWatermark = NSAttributedString(string: watermark, attributes: stringAttributes)
-            
-            image.draw(at: CGPoint(x: 0, y: 0))
-            
-            attributedWatermark.draw(with: CGRect(x: 30, y: 30, width: 150, height: 30), options: .usesLineFragmentOrigin, context: nil)
-        }
-        return renderedImage
-    }
-    
     @objc func shareTapped() {
-        guard let image = imageView.image else {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
             print("No image found")
             return
         }
-        
-        // Project 27, challenge 3:
-        let watermarkedImage = watermarked(image: image)
-        
-        let vc = UIActivityViewController(activityItems: [watermarkedImage, selectedImage!], applicationActivities: [])
+
+        let vc = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
